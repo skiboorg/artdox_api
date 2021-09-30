40fb7d5a45ae3808a1216934db6851c3eb216c79
skib <ss@ss.ss>
2021-09-30 18:23:28 +0300

    add  mobile banner

--- data/models.py
+++ data/models.py
@@ -4,3 +4,4 @@ from django.db import models
 
 class Banner(models.Model):
     image = models.ImageField('Баннер', upload_to='banner', blank=True, null=True)
+    image_mob = models.ImageField('Баннер мобильный', upload_to='banner', blank=True, null=True)
