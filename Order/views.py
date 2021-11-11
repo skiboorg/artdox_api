d89ec95498448fe84f0033b0d49fbf30139c2121
skiboorg <d@skib.org>
2021-11-11 10:19:27 +0300

    new cart & order

--- Order/views.py
+++ Order/views.py
@@ -6,7 +6,58 @@ from .models import *
 from Cart.models import *
 from user.models import *
 from .serializers import *
+import requests
+import setting
+import json
 
+def init_payment(order):
+    headers = {
+                  'Content-Type': 'application/json',
+                }
+    order_items = order.order_items.all()
+    order_user = order.user
+    items = []
+
+    for order_item in order_items:
+        items.append(
+            {
+                "Name": order_item.item.name,
+                "Price": order_item.item.price * 100,
+                "Quantity": order_item.amount,
+                "Amount": order_item.price * 100,
+                "PaymentMethod": "full_payment",
+                "PaymentObject": "commodity",
+                "Tax": "vat10",
+                "Ean13": "0123456789"
+            },
+        )
+
+    payload = {
+        "TerminalKey": setting.TERMINAL_ID,
+        "Amount": order.price * 100,
+        "OrderId": order.id,
+        "Description": f'Оплата заказа №{order.id}',
+        "NotificationURL": 'https://artdox.ru/api/order/payment_notify',
+        # "DATA": {
+        #     "Phone": "+71234567890",
+        #     "Email": "a@test.com"
+        # },
+        "Receipt": {
+            "Email": order.user.email,
+            "Phone": order.user.phone,
+            "EmailCompany": "b@test.ru",
+            "Taxation": "osn",
+            "Items": items
+        }
+    }
+    print(payload)
+    response = requests.post(setting.INIT_PAYMENT_URL,data=json.dumps(payload), headers=headers)
+    response_json = response.json()
+    if response_json['Success']:
+        print(response_json['PaymentURL'])
+        return {'success': True, 'payment_url':response_json['PaymentURL']}
+    else:
+        return {'success': False}
 
 class CreateOrder(APIView):
     def post(self,request):
@@ -24,20 +75,20 @@ class CreateOrder(APIView):
             item.item.left -= item.amount
             item.item.save()
             request.user.total_amount += item.amount
-            item.delete()
+            # item.delete()
 
-        cart.price = 0
-        cart.save()
-
-        request.user.total_summ += new_order.price
-        request.user.save()
-
-        Transaction.objects.create(user=self.request.user,
-                                   amount=new_order.price,
-                                   is_buy=True,
-                                   type='VISA')
-
-        return Response(status=200)
+        # cart.price = 0
+        # cart.save()
+        #
+        # request.user.total_summ += new_order.price
+        # request.user.save()
+        #
+        # Transaction.objects.create(user=self.request.user,
+        #                            amount=new_order.price,
+        #                            is_buy=True,
+        #                            type='CARD')
+        result = init_payment(new_order)
+        return Response(result, status=200)
 
 
 class GetOrders(generics.ListAPIView):
@@ -46,3 +97,13 @@ class GetOrders(generics.ListAPIView):
     def get_queryset(self):
         return Order.objects.filter(user=self.request.user)
 
+class PaymentNotify(APIView):
+    def get(self,request):
+        print('get')
+        print(self.request.query_params)
+        return Response(status=200)
+
+    def post(self, request):
+        print('post')
+        print(request.data)
+        return Response(status=200)
